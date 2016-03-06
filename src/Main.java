f6303fd649c25aa379ca3f9686d12d4f13344f36
Dalsaniya <mdn2836@springer-sbm.com>
2016-03-06 12:59:42 +0530

    Milan: added Main class and test class for main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mdn2836 on 3/6/2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
---
+++ test/MainTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mdn2836 on 3/6/2016.
+ */
+public class MainTest {
+
+    public void testMainMethod(){
+        
+    }
+}
